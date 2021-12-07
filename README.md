13dec9f77a527a8e94c2ff4fb230ad14352323b2
xiongjie.xue <xiongjie.xue@luckincoffee.com>
2021-12-07 18:39:18 +0800

    feat: 🎸 日历组件

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # calendar
 日历组件（Vue版本、微信小程序版本）
+
+
+## UI效果
+![20211207175255](https://s2.loli.net/2021/12/07/xo9aW68FwNACq7c.png)
+
+## 重点
+
+- `spot` 可以传入时间戳或者带有`/`格式 、`-`格式的字符串，默认ui是绿点，可根据需求传入slot自定义样式
+
+- 目前像素单位是小程序版本的`rpx`，可根据自己需求调整
+## Vue版本用法
+
+```html
+<calendar :spot="['2021/12/8']" @onSelect="dayChange" @onToggleOpen="openChange"/>
+```
+## 微信小程序版本用法
+
+```html
+<calendar spot="{{['2021/12/8']}}" bind:onSelect="dayChange" bind:onToggleOpen="openChange"/>
+```
+## Props type
+
+| _Prop_    | _Type_   | _Defaults_ | _Required_ | _Description_                                                      |
+| :-------- | :------- | :--------- | :--------- | ------------------------------------------------------------------ |
+|spot|Array|[]|×|设置绿点的时间，样式可自定义|
+|isShowSlotSpot|Boolean|false|×|是否显示slot的spot|
+|defaultTime|String|''|×|当前默认选中时间，不传则为当前日期`filters`|
+|onSelect|Function|()=>{year,month,day,dateString}|×|生成 Excel 前的回调函数|
+|onToggleOpen |Function|()=>true or false|×|生成 Excel 成功的回调函数|
---
+++ vue-calendar/index.vue
@@ -0,0 +1,360 @@
+<!--
+ * @Description: Vue日历组件
+ * @Author: Xiongjie.Xue(xiongjie.xue@luckincoffee.com)
+ * @Date: 2021-12-07 17:40:33
+ * @LastEditors: Xiongjie.Xue(xiongjie.xue@luckincoffee.com)
+ * @LastEditTime: 2021-12-07 18:37:51
+-->
+<template>
+  <view class="component-common-calendar">
+    <view class="title">
+      <view class="title-name">
+        {{ selectDay.year }}.{{ selectDay.month > 9 ? selectDay.month : "0" + selectDay.month }}
+        <view class="icon" @click="lastMonth">👆</view>
+        <view class="icon" @click="nextMonth">👇</view>
+      </view>
+      <view class="title-open" @click="openChange">
+        <view>{{ open ? "收起" : "展开" }}</view>
+      </view>
+    </view>
+    <!-- 日历头部 -->
+    <view class="calendar-week">
+      <view class="view">日</view>
+      <view class="view">一</view>
+      <view class="view">二</view>
+      <view class="view">三</view>
+      <view class="view">四</view>
+      <view class="view">五</view>
+      <view class="view">六</view>
+    </view>
+    <!-- 日历主体 -->
+    <view class="calendar-main" :style="{height : `${dateList.length/7*72}rpx`}">
+      <view v-for="(item,index) in dateList" :key="index" class="day">
+        <view
+          :class="['bg',(item.year === selectDay.year && item.month === selectDay.month) ? (item.day === selectDay.day ? 'select' : '') : 'other-month']"
+          @click="selectChange(item)"
+        >{{ item.day }}</view>
+        <view v-if="item.spot && !isShowSlotSpot" class="spot" />
+        <slot name="spot" />
+      </view>
+    </view>
+  </view>
+</template>
+<script>
+export default {
+  props: {
+    spot: {
+      type: Array,
+      default: () => [],
+    },
+    isShowSlotSpot: {
+      type: Boolean,
+      default: false,
+    },
+    defaultTime: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      open: true,
+      dateList: [], // 日历主体渲染数组
+      selectDay: {}, // 选中时间
+    };
+  },
+  created() {
+    const now = this.defaultTime ? new Date(this.defaultTime) : new Date();
+    const selectDay = {
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      day: now.getDate(),
+      dateString: this.formatTime(now, "Y-M-D"),
+    };
+    this.setMonth(selectDay.year, selectDay.month, selectDay.day);
+  },
+  methods: {
+    formatNumber(n) {
+      n = n.toString();
+      return n[1] ? n : `0${n}`;
+    },
+    getDate(time, format) {
+      const formateArr = ["Y", "M", "D", "h", "m", "s"];
+      const returnArr = [];
+      const date = new Date(time);
+      returnArr.push(date.getFullYear());
+      returnArr.push(this.formatNumber(date.getMonth() + 1));
+      returnArr.push(this.formatNumber(date.getDate()));
+      returnArr.push(this.formatNumber(date.getHours()));
+      returnArr.push(this.formatNumber(date.getMinutes()));
+      returnArr.push(this.formatNumber(date.getSeconds()));
+      returnArr.forEach((v, i) => {
+        format = format.replace(formateArr[i], v);
+      });
+      return format;
+    },
+    getDateDiff(time) {
+      let r = "";
+      const ft = new Date(time);
+      const nt = new Date();
+      const nd = new Date(nt);
+      nd.setHours(23);
+      nd.setMinutes(59);
+      nd.setSeconds(59);
+      nd.setMilliseconds(999);
+      const d = parseInt((nd - ft) / 86400000);
+      switch (true) {
+        case d === 0: {
+          const t = parseInt(nt / 1000) - parseInt(ft / 1000);
+          switch (true) {
+            case t < 60:
+              r = "刚刚";
+              break;
+            case t < 3600:
+              r = `${parseInt(t / 60)}分钟前`;
+              break;
+            default:
+              r = `${parseInt(t / 3600)}小时前`;
+          }
+          break;
+        }
+        case d === 1: {
+          r = "昨天";
+          break;
+        }
+        case d === 2: {
+          r = "前天";
+          break;
+        }
+        case d > 2 && d < 30: {
+          r = `${d}天前`;
+          break;
+        }
+        default:
+          r = getDate(time, "Y-M-D");
+      }
+      return r;
+    },
+    /**
+     * 时间戳转化为年 月 日 时 分 秒
+     * time: 需要被格式化的时间，可以被new Date()解析即可
+     * format：格式化之后返回的格式，年月日时分秒分别为Y, M, D, h, m, s，这个参数不填的话则显示多久前
+     */
+    formatTime(time, format) {
+      if (!format) {
+        return this.getDateDiff(time);
+      }
+      return this.getDate(time, format);
+    },
+    // 上月切换按钮点击
+    lastMonth() {
+      const lastMonth = new Date(this.selectDay.year, this.selectDay.month - 2);
+      const year = lastMonth.getFullYear();
+      const month = lastMonth.getMonth() + 1;
+      this.setMonth(year, month);
+    },
+    // 下月切换按钮点击
+    nextMonth() {
+      const nextMonth = new Date(this.selectDay.year, this.selectDay.month);
+      const year = nextMonth.getFullYear();
+      const month = nextMonth.getMonth() + 1;
+      this.setMonth(year, month);
+    },
+    // 设置月份
+    setMonth(setYear, setMonth, setDay) {
+      if (
+        this.selectDay.year !== setYear ||
+        this.selectDay.month !== setMonth
+      ) {
+        const day = Math.min(
+          new Date(setYear, setMonth, 0).getDate(),
+          this.selectDay.day
+        );
+        const time = new Date(setYear, setMonth - 1, setDay || day);
+
+        this.selectDay = {
+          year: setYear,
+          month: setMonth,
+          day: setDay || day,
+          dateString: this.formatTime(time, "Y-M-D"),
+        };
+        if (!setDay) {
+          this.open = true;
+        }
+
+        this.dateInit(setYear, setMonth);
+        this.setSpot();
+        this.$emit("onSelect", this.selectDay);
+      }
+    },
+    // 展开收起
+    openChange() {
+      this.open = !this.open;
+      this.$emit("onToggleOpen", this.open);
+      this.dateInit();
+      this.setSpot();
+    },
+    // 设置日历底下是否展示小圆点
+    setSpot() {
+      const timeArr = this.spot.map((item) => {
+        return this.formatTime(item, "Y-M-D");
+      });
+      this.dateList = this.dateList.map((item) => {
+        if (timeArr.indexOf(item.dateString) !== -1) {
+          item.spot = true;
+        } else {
+          item.spot = false;
+        }
+        return item;
+      });
+    },
+    // 日历主体的渲染方法
+    dateInit(setYear = this.selectDay.year, setMonth = this.selectDay.month) {
+      const dateList = []; // 需要遍历的日历数组数据
+      const now = new Date(setYear, setMonth - 1); // 当前月份的1号
+      const startWeek = now.getDay(); // 目标月1号对应的星期
+      const dayNum = new Date(setYear, setMonth, 0).getDate(); // 当前月有多少天
+      const forNum = Math.ceil((startWeek + dayNum) / 7) * 7; // 当前月跨越的周数
+      if (this.open) {
+        // 展开状态，需要渲染完整的月份
+        for (let i = 0; i < forNum; i++) {
+          const now2 = new Date(now);
+          now2.setDate(i - startWeek + 1);
+          let obj = {};
+          obj = {
+            day: now2.getDate(),
+            month: now2.getMonth() + 1,
+            year: now2.getFullYear(),
+            dateString: this.formatTime(now2, "Y-M-D"),
+          };
+          dateList[i] = obj;
+        }
+      } else {
+        // 非展开状态，只需要渲染当前周
+        for (let i = 0; i < 7; i++) {
+          const now2 = new Date(now);
+          // 当前周的7天
+          now2.setDate(
+            Math.ceil((this.selectDay.day + startWeek) / 7) * 7 -
+              6 -
+              startWeek +
+              i
+          );
+          let obj = {};
+          obj = {
+            day: now2.getDate(),
+            month: now2.getMonth() + 1,
+            year: now2.getFullYear(),
+            dateString: this.formatTime(now2, "Y-M-D"),
+          };
+          dateList[i] = obj;
+        }
+      }
+      this.dateList = dateList;
+    },
+    // 一天被点击时
+    selectChange(item) {
+      const { year, month, day, dateString } = item;
+      const selectDay = {
+        year,
+        month,
+        day,
+        dateString,
+      };
+      if (this.selectDay.year !== year || this.selectDay.month !== month) {
+        this.setMonth(year, month, day);
+      } else if (this.selectDay.day !== day) {
+        this.selectDay = selectDay;
+        this.$emit("onSelect", this.selectDay);
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.component-common-calendar {
+  background-color: #fff;
+  .title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 40rpx;
+    color: #333;
+    padding: 30rpx;
+    line-height: 60rpx;
+    .title-name,
+    .title-open {
+      display: flex;
+      align-items: center;
+    }
+    .year-month {
+      margin-right: 20rpx;
+    }
+    .icon {
+      padding: 0 16rpx;
+      font-size: 32rpx;
+      color: #999;
+    }
+    .title-open {
+      background-color: #f6f6f6;
+      color: #999;
+      font-size: 22rpx;
+      line-height: 36rpx;
+      border-radius: 18rpx;
+      padding: 0 14rpx;
+    }
+  }
+  .calendar-week {
+    display: flex;
+    line-height: 40rpx;
+    padding: 0 25rpx;
+    font-size: 28rpx;
+    color: #999;
+    .view {
+      width: 100rpx;
+      text-align: center;
+    }
+  }
+
+  .calendar-main {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 30rpx 25rpx;
+    transition: height 0.3s;
+    overflow: hidden;
+    .day {
+      position: relative;
+      width: 100rpx;
+      color: #666;
+      text-align: center;
+      height: 72rpx;
+      .bg {
+        height: 56rpx;
+        line-height: 56rpx;
+        font-size: 28rpx;
+        color: #333;
+        font-weight: bold;
+      }
+      .select {
+        width: 56rpx;
+        border-radius: 50%;
+        text-align: center;
+        color: #fff;
+        background: linear-gradient(-60deg, #0fdac5, #1bc7b0);
+        box-shadow: 0px 5px 16px 0px #c6f3ed;
+        margin: 0 auto;
+      }
+      .other-month {
+        color: #ccc;
+      }
+      .spot {
+        width: 8rpx;
+        height: 8rpx;
+        background-color: #1dcdb8;
+        border-radius: 50%;
+        margin: 6rpx auto 0;
+      }
+    }
+  }
+}
+</style>
